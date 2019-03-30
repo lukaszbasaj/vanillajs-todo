8b81df25ab5891eb708a66dd51cfb8ed36e98ffa
Łukasz Basaj <basaj.lukasz@gmail.com>
2019-03-30 23:39:01 +0100

    added application logic and UI

--- src/scripts/Todo.js
+++ src/scripts/Todo.js
@@ -1,43 +1,173 @@
 let mockData = [{
-        id: '1',
-        title: 'This is title',
-        done: false,
-        date: new Date()
-    },
-    {
-        id: '2',
-        title: 'This is second title',
-        done: false,
-        date: new Date()
-    },
-    {
-        id: '3',
-        title: 'This is third title',
-        done: false,
-        date: new Date()
-    },
-    {
-        id: '4',
-        title: 'This is fourth title',
-        done: false,
-        date: new Date()
-    }
-];
-
-
-
-
+    id: '1',
+    title: 'This is fist task',
+    done: false,
+    date: new Date()
+}, {
+    id: '2',
+    title: 'This is second task',
+    done: false,
+    date: new Date()
+}, {
+    id: '3',
+    title: 'This is third task',
+    done: false,
+    date: new Date()
+}, {
+    id: '4',
+    title: 'This is forth task',
+    done: false,
+    date: new Date()
+}];
 
 class Todo {
+
     constructor() {
+        let self = this;
+
         this.list = document.querySelector('.list-items');
+        this.render();
+
+        document.querySelector('.btn-add-item').addEventListener('click', this.insertItem.bind(this));
+        document.querySelector('.btn-update').addEventListener('click', this.updateItem.bind(this));
+
+        document.addEventListener('click', event => {
+            if (!event.target) {
+                return;
+            }
+
+            if (event.target.classList.contains('btn-delete')) {
+                self.removeItem(event);
+            }
+
+            if (event.target.classList.contains('btn-edit')) {
+                self.renderEditForm(event);
+            }
+
+            if (event.target.classList.contains('btn-complete')) {
+                self.setTaskComplete(event);
+            }
+        });
+    }
+
+    render() {
+        this.list.innerHTML = '';
+
+        mockData.forEach(item => {
+            this.createDomElements(item.id);
+            this.li.insertAdjacentHTML('afterbegin', item.title);
+
+            if (item.done) {
+                this.li.classList.add('done');
+            }
+
+            this.list.appendChild(this.li);
+        });
+    }
+
+    renderEditForm(event) {
+        let id = event.target.getAttribute('data-id');
+
+        document.querySelector('.edit-popup').classList.remove('hide');
+        document.querySelector('.edit-popup').classList.add('show');
+        document.querySelector('.btn-update').setAttribute('data-id', id);
+
+        mockData.forEach(item => {
+            if (item.id === id) {
+                document.querySelector('.edit-item').value = item.title;
+            }
+        });
+    }
+
+    createDomElements(id) {
+        this.li = document.createElement('li');
+        this.edit = document.createElement('button');
+        this.delete = document.createElement('button');
+        this.complete = document.createElement('button');
+
+        const liClassExtend = ['li', 'items', 'list-group-item']
+        this.li.classList.add(...liClassExtend);
+
+        const btnClassessEdit = ['btn-edit', 'btn', 'btn-outline-primary', 'btn-sm'];
+        const btnClassessDelete = ['btn-delete', 'btn', 'btn-outline-danger', 'btn-sm'];
+        const btnClassessComplete = ['btn-complete', 'btn', 'btn-outline-success', 'btn-sm'];
+
+        this.edit.classList.add(...btnClassessEdit);
+        this.delete.classList.add(...btnClassessDelete);
+        this.complete.classList.add(...btnClassessComplete);
+
+        this.delete.setAttribute('data-id', id);
+        this.edit.setAttribute('data-id', id);
+        this.complete.setAttribute('data-id', id);
+
+        this.edit.innerHTML = 'Edit';
+        this.delete.innerHTML = 'Delete';
+        this.complete.innerHTML = 'Complete';
+
+        this.li.appendChild(this.delete);
+        this.li.appendChild(this.edit);
+        this.li.appendChild(this.complete);
+    }
+
+    insertItem() {
+        let todoItem = document.querySelector('.item').value;
+
+        let newItem = {
+            id: Date.now().toString(),
+            title: todoItem,
+            done: false,
+            date: new Date()
+        };
+
+        mockData.push(newItem);
+
+        document.querySelector('.item').value = '';
         this.render();
     }
 
+    removeItem(event) {
+        let id = event.target.getAttribute('data-id');
 
+        mockData = mockData.filter(item => {
+            if (item.id !== id) {
+                return item;
+            }
+        });
 
+        this.render();
+    }
 
+    updateItem(event) {
+        let id = event.target.getAttribute('data-id');
+        let itemTobeUpdated = document.querySelector('.edit-item').value;
+
+        mockData = mockData.map(item => {
+            if (item.id === id) {
+                item['title'] = itemTobeUpdated;
+            }
+
+            return item;
+        });
+
+        document.querySelector('.edit-popup').classList.remove('show');
+        document.querySelector('.edit-popup').classList.add('hide');
+
+        this.render();
+    }
+
+    setTaskComplete(event) {
+        let id = event.target.getAttribute('data-id');
+
+        mockData = mockData.map(item => {
+            if (item.id === id) {
+                item['done'] = true;
+            }
+
+            return item;
+        });
+
+        this.render();
+    }
 }
-
 
 export default Todo;
